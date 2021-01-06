14890912365b288e6d492fa2d419df87de1f50fc
Redbow <tristanleroy26@gmail.com>
2021-01-06 11:43:41 +0100

    feat: github stats

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,9 +9,14 @@
             </p>
         </div>
         <p>
-            Développeur
+            Développeur / Api scientist
         </p>
         <hr />
+        <p>Statistique github</p>
+        <div class="github">
+            <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=redbow26&hide=Blade&exclude_repo=Projet-S2&langs_count=5&theme=vue-dark" alt="github statistique">
+            <img align="center" src="https://github-readme-stats.vercel.app/api/?username=redbow26&theme=vue-dark" alt="github statistique">
+        </div>
     </div>
 </template>
 
